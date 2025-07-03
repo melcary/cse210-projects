e2451b8a6e353b99d58d787e4ee3ea98f45466ad
melcary <melcary13@gmail.com>
2025-07-03 13:09:43 -0500

    Finished prep3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        //Part1
+        //Console.Write("What is the magic number? ");
+        //int num = int.Parse(Console.ReadLine());
+
+        Random randomNum = new Random();
+        int num = randomNum.Next(1, 101);
+
+        int response = -1;
+
+        while (response != num)
+        {
+            Console.Write("What is your guess? ");
+            response = int.Parse(Console.ReadLine());
+
+            if (response > num)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (response < num)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You gessed it!");
+            }
+        }
     }
 }
